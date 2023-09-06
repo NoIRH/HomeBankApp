7a3f8fa82fa8f42d3a54f65622253cce67050806
TUNO\kikut <kikutepsilon@gmail.com>
2023-09-06 14:06:31 +0300

    Added ManagerOperation class

---
+++ HomeBankApp/ManagerOperation.cs
@@ -0,0 +1,36 @@
+﻿
+namespace HomeBankApp
+{
+    internal static class ManagerOperation
+    {
+        public static double AddMoney(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double RemoveMoney(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetTotalMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetOperationsByCurrentMonth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetOperationsByCurrentYearAdd()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetOperationsByCurrentYearRemove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
