b9e92b4e72d01c280d7fcb9e9b8806e5a59b3b5a
Виктор Суржко <deminion0202@gmail.com>
2023-09-07 20:37:38 +0300

    add models and db manager

---
+++ HomeBankApp/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using HomeBankApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBankApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Operation> Operations { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ HomeBankApp/DbManager.cs
@@ -0,0 +1,45 @@
+﻿using HomeBankApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBankApp
+{
+    public class DbManager
+    {
+        ApplicationContext Context = new ApplicationContext();
+        public void InitializeDb() 
+        {
+            Context.Database.EnsureCreated();
+            Context.Users.Load();
+            Context.Operations.Load();
+        }
+        public IEnumerable<User> GetUsers() => Context.Users;
+        public IEnumerable<Operation> GetOperations() => Context.Operations;
+        public void AddUser(User user)
+        {
+            Context.Users.Add(user);
+            SaveChanges();
+        }
+        public void RemoveUser(User user)
+        {
+            Context.Users.Remove(user);
+            SaveChanges();
+        }
+        public void AddOperation(Operation operation)
+        {
+            Context.Operations.Add(operation);
+            SaveChanges();
+        }
+        public void RemoveOperation(Operation operation)
+        {
+            Context.Operations.Remove(operation);
+            SaveChanges();
+        }
+        public void SaveChanges() => Context.SaveChanges();
+
+    }
+}
