6f93f8228686498f599165b136417b219da2b363
Georg1309 <gornoi123@mail.ru>
2023-09-27 01:35:52 +0300

    Регистрация и авторизация

---
+++ HomeBankApp/LoginForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace HomeBankApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            Registrlable = new Label();
+            button1 = new Button();
+            PasswordText = new TextBox();
+            LoginText = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(Registrlable);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(PasswordText);
+            panel1.Controls.Add(LoginText);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(475, 446);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // Registrlable
+            // 
+            Registrlable.AutoSize = true;
+            Registrlable.Location = new Point(158, 404);
+            Registrlable.Name = "Registrlable";
+            Registrlable.Size = new Size(151, 20);
+            Registrlable.TabIndex = 6;
+            Registrlable.Text = "Зарегистрироваться";
+            Registrlable.MouseClick += Registrable_MouseClick;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(150, 354);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 47);
+            button1.TabIndex = 5;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // PasswordText
+            // 
+            PasswordText.Location = new Point(94, 270);
+            PasswordText.Multiline = true;
+            PasswordText.Name = "PasswordText";
+            PasswordText.Size = new Size(268, 36);
+            PasswordText.TabIndex = 4;
+            // 
+            // LoginText
+            // 
+            LoginText.Location = new Point(94, 175);
+            LoginText.Multiline = true;
+            LoginText.Name = "LoginText";
+            LoginText.Size = new Size(268, 36);
+            LoginText.TabIndex = 3;
+            LoginText.TextChanged += LoginText_TextChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.pass;
+            pictureBox2.Location = new Point(24, 259);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(53, 59);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.login;
+            pictureBox1.Location = new Point(24, 163);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(53, 58);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(224, 224, 224);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(475, 104);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(24, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(427, 81);
+            label1.TabIndex = 0;
+            label1.Text = "Авторизация";
+            label1.Click += label1_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 446);
+            Controls.Add(panel1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Button button1;
+        private TextBox PasswordText;
+        private TextBox LoginText;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label Registrlable;
+    }
+}
---
+++ HomeBankApp/RegistrationForm1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeBankApp
+{
+    public partial class RegistrationForm1 : Form
+    {
+        public RegistrationForm1()
+        {
+            InitializeComponent();
+            Size = new Size(493, 493);
+
+        }
+
+        private void RegistrationForm1_Load(object sender, EventArgs e)
+        {
+            this.Width = 493;
+            this.Height = 493;
+
+        }
+
+        private void label2_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+}
