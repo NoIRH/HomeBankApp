b96079be1b9b656abb6ce233a3f9a2a4bdceedd4
Victor <67371337+Gveideon@users.noreply.github.com>
2023-09-26 19:56:53 +0300

    реализация FileContext

--- HomeBankApp/Contexts/FileContext.cs
+++ HomeBankApp/Contexts/FileContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeBankApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace HomeBankApp.Contexts
 {
     public class FileContext
     {
+        public List<User> Users { get; set; }
+        public List<Operation> Operations { get; set; }
     }
 }
